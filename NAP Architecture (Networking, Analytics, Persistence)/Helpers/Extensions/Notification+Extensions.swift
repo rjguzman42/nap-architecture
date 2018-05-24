74f28d962403a349ad1feb0eacea3bf80172e1f0
Roberto Guzman <35378117+rjguzman42@users.noreply.github.com>
2018-05-23 20:56:04 -0400

    Added Helpers

---
+++ NAP Architecture (Networking, Analytics, Persistence)/Helpers/Extensions/Notification+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  Notification+Extensions.swift
+//  NetworkingCourse
+//
+//  Created by Roberto Guzman on 4/9/18.
+//  Copyright © 2018 Roberto Guzman. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let DownloadImage = Notification.Name("DownloadImageNotification")
+}
